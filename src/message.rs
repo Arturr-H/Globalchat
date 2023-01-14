0ba92ce39fe801be39a835ef8f225f3a02c6b132
Artur <artur.h.0427@gmail.com>
2023-01-14 02:08:16 +0100

    Create `Response` struct

--- src/message.rs
+++ src/message.rs
@@ -2,7 +2,9 @@
 #![allow(dead_code)]
 
 /* Imports */
+use std::time::SystemTime;
 use serde::{ Deserialize, Serialize };
+use uuid::Uuid;
 
 /* Request is the user request (in JSON) which contains important data */
 #[derive(Deserialize)]
@@ -12,9 +14,22 @@ pub struct Request {
 
     /// Content as bytes
     content: Vec<u8>,
+}
+
+/* Response */
+#[derive(Serialize)]
+pub struct Response {
+    /// User suid
+    suid: String,
+
+    /// Content as bytes
+    content: Vec<u8>,
 
     /// Date sent
     date: usize,
+
+    /// Message ID (generated server-side)
+    id: String
 }
 
 /* ClientMessage is the usermessage, with additional information like id which is created server-side */
@@ -37,7 +52,7 @@ pub struct ClientMessage {
 impl Into<ClientMessage> for Request {
     fn into(self) -> ClientMessage {
         ClientMessage {
-            date: self.date,
+            date: 1,
             client: self.client,
             content: self.content,
             id: ClientMessage::gen_id()
@@ -49,13 +64,18 @@ impl Into<ClientMessage> for Request {
 impl ClientMessage {
     /* Create message ID */
     pub fn gen_id() -> String {
-        //! TODO: Make this actually generate an ID
-        String::new()
+        Uuid::new_v4().hyphenated().to_string()
     }
 
     /* Convert to JSON */
-    pub fn to_json(&self) -> Option<String> {
-        serde_json::to_string(&self).ok()
+    pub fn to_stripped_json(self) -> Option<String> {
+        serde_json::to_string(
+            //TODO: FIX SUID
+            &Response::from_client_message(
+                self,
+                String::new()
+            )
+        ).ok()
     }
 
     /* Getters */
@@ -63,4 +83,20 @@ impl ClientMessage {
     pub fn content(&self) -> &Vec<u8> { &self.content }
     pub fn date(&self) -> usize { self.date }
     pub fn id(&self) -> &String { &self.id }
+}
+
+/* Implementations for Response */
+impl Response {
+    pub fn from_client_message(message:ClientMessage, suid:String) -> Self {
+        Self {
+            suid,
+            content: message.content,
+            date: message.date,
+            id: message.id
+        }
+    }
+}
+
+fn get_sys_time_in_secs() -> u128 {
+    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis()
 }
