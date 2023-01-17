04546ff819560691d09ea72e70160633a8b4012a
Artur <artur.h.0427@gmail.com>
2023-01-17 21:04:21 +0100

    Peer map added to main, now supports sending to multiple clients

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+#![allow(dead_code, unused_imports)]
+
 /* Imports */
 use dotenv::dotenv;
 use lazy_static::lazy_static;
 use std::{
-    net::TcpListener,
-    thread::spawn,
-    env::var
+    collections::HashMap,
+    env::var,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
 };
 
-use tokio_tungstenite::{ tungstenite::{ accept, Message } };
-use crate::incoming::handle_message;
+use futures_channel::mpsc::{ unbounded, UnboundedSender };
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
 
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::{ protocol::Message };
+
+use crate::incoming::handle_message;
 mod incoming;
 mod message;
 
@@ -20,7 +27,12 @@ lazy_static! {
     pub static ref ACCOUNT_MANAGER_URL:String = var("ACCOUNT_MANAGER_URL").unwrap();
 }
 
-fn main() {
+/* Types */
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+#[tokio::main]
+async fn main() {
     dotenv().unwrap();
 
     /* Load lazy initialized env varaibles to prewarn about missing vars */
@@ -28,37 +40,68 @@ fn main() {
     
     /* Initialize stream */
     let addr = format!("{ADDRESS}:{PORT}");
-    let server = TcpListener::bind(&addr).unwrap();
+    let server = TcpListener::bind(&addr).await.unwrap();
     println!("{addr}");
 
+    /* Create peer map */
+    let peers:PeerMap = Arc::new(Mutex::new(HashMap::new()));
+
     /* Incoming requests */
-    for stream in server.incoming() {
-        spawn(move || {
-            let mut websocket = match accept(match stream {
-                Ok(e) => e,
-                Err(_) => return
-            }) {
-                Ok(e) => e,
-                Err(_) => return
-            };
+    while let Ok((stream, addr)) = server.accept().await {
+        tokio::spawn(handle_connection(peers.clone(), stream, addr));
+    }
+}
 
-            /* Message loop */
-            loop {
-                let msg = match handle_message(match websocket.read_message() {
-                    Ok(e) => e,
-                    Err(_) => continue
-                }) {
-                    Ok(e) => e,
-                    Err(_) => continue
-                };
-
-                /* Write to client */
-                websocket.write_message(
-                    Message::Text(
-                        msg.to_stripped_json().unwrap()
-                    )
-                ).unwrap();
-            };
-        });
+async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
+    let ws_stream = match tokio_tungstenite::accept_async(raw_stream).await {
+        Ok(e) => e,
+        Err(_) => return
     };
+
+    /* Insert to peers */
+    let (tx, rx) = unbounded();
+    match peer_map.lock() { Ok(e) => e, Err(_) => return }.insert(addr, tx);
+    let (outgoing, incoming) = ws_stream.split();
+
+    /* Message loop */
+    let broadcast_incoming = incoming.try_for_each(|data| {
+        let peers = match peer_map.lock() {
+            Ok(e) => e,
+            Err(_) => return future::ok(())
+        };
+        let message = match handle_message(data) {
+            Ok(e) => e,
+            Err(_) => return future::ok(())
+        };
+
+        /* Write to all clients including ourselves */
+        for recp in peers.iter().map(|(_, ws_sink)| ws_sink) {
+            match recp.unbounded_send(
+                Message::Text(
+                    match message
+                        .clone()
+                        .to_stripped_json() {
+                            Some(e) => e,
+                            None => return future::ok(())
+                        }
+                )
+            ) {
+                Ok(e) => e,
+                Err(_) => return future::ok(())
+            }
+        }
+
+        future::ok(())
+    });
+
+
+    /* Enable user to also recieve messages */
+    let receive_from_others = rx.map(Ok).forward(outgoing);
+    pin_mut!(broadcast_incoming, receive_from_others);
+
+    /* Run recv / broadcast */
+    future::select(broadcast_incoming, receive_from_others).await;
+
+    /* Remove peer */
+    peer_map.lock().unwrap().remove(&addr);
 }
