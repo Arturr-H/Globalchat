12333dc816d0db9c14280498e413e0a26bc9e692
Artur <artur.h.0427@gmail.com>
2023-01-18 17:35:57 +0100

    Add text input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
+import React from "react";
+import Message from "./Message";
 
-function App() {
-	return (
-		<div className="App">
-		</div>
-	);
+class App extends React.PureComponent {
+	constructor(props) {
+		super(props);
+
+		/* Changeable */
+		this.state = {
+			messages: [{ content: "hej" }],
+			users: [],
+			inputData: ""
+		};
+
+		/* Refs */
+		this.input = React.createRef();
+	}
+
+	/* Render */
+	render() {
+		return (
+			<main>
+				<section>
+					{this.state.messages.map(message => <Message {...message} />)}
+				</section>
+				<input
+					type="text"
+					placeholder="Skriv nåt..."
+					className="chat-input"
+					ref={this.input}
+				/>
+			</main>
+		);
+	}
 }
 
 export default App;
