3aa63dd0564531028d978996226a28ec28b1eb5b
Artur <artur.h.0427@gmail.com>
2023-01-18 20:14:47 +0100

    Implement for chatting now done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,13 +9,70 @@ class App extends React.PureComponent {
 
 		/* Changeable */
 		this.state = {
-			messages: [{ content: "hej" }],
+			messages: [{ content: "hej" }, { content: "hej" }],
 			users: [],
 			inputData: ""
 		};
 
 		/* Refs */
 		this.input = React.createRef();
+
+		/* Static */
+		this.ws = null;
+	}
+
+	/* Functions */
+	submitText = () => {
+		if (this.ws === null) { return };
+		console.log("a");
+		let text = this.state.inputData;
+
+		/* Clear text input */
+		this.setState({ inputData: "" });
+
+		/* Send text */
+		if (text.length > 0) {
+			this.ws.send(JSON.stringify({
+				content: this.encodeItems(text),
+				client: "aaaaawihdoahwodihaowihd"
+			}));
+		}
+	}
+
+	componentDidMount() {
+		this.ws = new WebSocket("ws://localhost:8080/");
+
+		if (this.ws !== null) {
+			this.ws.onopen = () => {
+				console.log('WebSocket connected');
+			};
+			this.ws.onmessage = this.handleMessage;
+			this.ws.onclose = () => {
+				console.log('WebSocket disconnected');
+			};
+		};
+	}
+	componentWillUnmount() {
+		this.ws.close();
+	}
+
+	/* Websocket functions */
+	handleMessage = (event) => {
+		let json = JSON.parse(event.data);
+		json["content"] = this.convertToRealContent(json["content"]);
+
+		/* Push message */
+		this.setState({ messages: [...this.state.messages, json] });
+	}
+
+	/*- Convert bytes into real text strings -*/
+	convertToRealContent = (content) => {
+		const decoder = new TextDecoder();
+		return decoder.decode(new Uint8Array(content));
+	};
+	encodeItems = (content) => {
+		let utf8Encode = new TextEncoder();
+		return Array.from(utf8Encode.encode(content));
 	}
 
 	/* Render */
@@ -26,18 +83,23 @@ class App extends React.PureComponent {
 					{this.state.messages.map(message => <Message {...message} />)}
 				</section>
 				<div className="input">
-					<button className="add-button">
-						<Icon size={32} dark icon="paperclip" />
-					</button>
-					<input
-						type="text"
-						placeholder="Skriv nåt..."
-						className="chat-input"
-						ref={this.input}
-					/>
-					<button className="send-button">
-						<Icon size={32} dark icon="forward" />
-					</button>
+					<form onSubmit={(e) => { e.preventDefault(); this.submitText(); }}>
+						<button className="add-button">
+							<Icon size={32} dark icon="paperclip" />
+						</button>
+
+						<input
+							type="text"
+							placeholder="Skriv nåt..."
+							className="chat-input"
+							ref={this.input}
+							value={this.state.inputData}
+							onChange={(e) => this.setState({ inputData: e.target.value })}
+						/>
+						<button onClick={this.submitText} className="send-button">
+							<Icon size={32} dark icon="forward" />
+						</button>
+					</form>
 				</div>
 			</main>
 		);
--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -21,7 +21,7 @@ class Message extends React.PureComponent {
                 </div>
                 <div className="main">
                     <p className="data">@username - 20:12:21</p>
-                    <p className="content">Ad adipisicing est cillum aliquip excepteur occaecat ex. Excepteur incididunt nulla et magna. Dolor amet nisi magna laboris laborum labore id tempor ullamco enim occaecat et est dolore. Nostrud aliquip eiusmod elit nulla ex esse. Cillum minim laboris aliquip consequat eiusmod officia reprehenderit elit. Anim eu do in adipisicing et incididunt amet enim ut dolore et.Velit incididunt aliquip consequat tempor tempor pariatur consequat consequat amet anim adipisicing consequat pariatur. Quis fugiat velit nisi minim consequat fugiat elit id. Esse fugiat ut laborum pariatur velit voluptate deserunt aute sint ad. Eiusmod nisi amet culpa amet ad enim velit velit laboris aute commodo. Adipisicing voluptate exercitation culpa cillum velit est nisi cupidatat excepteur tempor incididunt irure.Aliqua anim minim adipisicing in eu aliqua dolor nisi qui. Est velit ea elit cupidatat minim laboris culpa officia fugiat. Veniam consequat sit veniam consequat aute labore deserunt. Fugiat est aute laborum sint labore pariatur ea incididunt in nisi aliqua ut. Amet anim laborum dolor aliqua elit incididunt. Pariatur adipisicing fugiat amet ullamco adipisicing enim ad consequat ullamco. Ut nulla est ex ex est do exercitation ipsum in exercitation qui nulla irure irure.</p>
+                    <p className="content">{this.props.content}</p>
                 </div>
             </div>
 		);
